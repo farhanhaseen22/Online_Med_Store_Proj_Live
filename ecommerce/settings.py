c970bff4cf418255dcefb8055576a6f35ed8fdcc
Farhan Haseen <farhan.haseen.22@gmail.com>
2023-12-03 19:01:16 -0330

    cors header for JS based requesting

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "corsheaders",
     
     'store.apps.StoreConfig',
     'accounts.apps.AccountsConfig',
@@ -55,6 +56,7 @@ AUTHENTICATION_BACKENDS = (
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
